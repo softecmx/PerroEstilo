bd19a40b5a7ad1a3ae2f7ca2cc6d3512312e2016
KevinSanchez-Cat <por_salon@hotmail.com>
2021-11-20 20:16:08 -0600

    theme y loggeo

--- app/src/main/java/com/programacion/perroestilocliente/RecuperarPasswordActivity.java
+++ app/src/main/java/com/programacion/perroestilocliente/RecuperarPasswordActivity.java
@@ -1,14 +1,134 @@
 package com.programacion.perroestilocliente;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RecuperarPasswordActivity extends AppCompatActivity {
+
+    com.google.android.material.textfield.TextInputEditText editCorreo;
+    Button btnEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recuperar_password);
+        editCorreo=findViewById(R.id.editForgotEmail);
+        btnEnviar=findViewById(R.id.btnForgotEnviar);
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(validar()){
+                    enviarCorreo();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent=new Intent(RecuperarPasswordActivity.this,LogginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void enviarCorreo() {
+        FirebaseAuth auth=FirebaseAuth.getInstance();
+        String email=editCorreo.getText().toString().trim();
+
+        auth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            mostarToast("Correo enviado",0,true);
+                            Intent intent=new Intent(RecuperarPasswordActivity.this,LogginActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }else{
+                            mostarToast("Error al enviar correo",3,true);
+                        }
+                    }
+                });
+
+    }
+
+    private boolean validar() {
+        String email=editCorreo.getText().toString().trim();
+        if(email.isEmpty()||!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editCorreo.setError("Correo no valido");
+            return false;
+        }
+        return true;
+    }
+    public void mostarToast(String txt, int estatus, boolean corto) {
+        LayoutInflater inflater = getLayoutInflater();
+
+        View layout = null;
+
+        if(estatus==0){
+            //Default
+            layout = inflater.inflate(R.layout.custom_toast_info,
+                    (ViewGroup) findViewById(R.id.layout_base));
+
+        }else if(estatus==1){
+            //Success
+            layout = inflater.inflate(R.layout.custom_toast_success,
+                    (ViewGroup) findViewById(R.id.layout_base));
+
+        }else if(estatus==2) {
+            //Warning
+            layout = inflater.inflate(R.layout.custom_toast_warning,
+                    (ViewGroup) findViewById(R.id.layout_base));
+
+        }else if(estatus==3){
+            //Error
+            layout = inflater.inflate(R.layout.custom_toast_error,
+                    (ViewGroup) findViewById(R.id.layout_base));
+
+
+        }else if(estatus==4){
+            //Falla de red
+            layout = inflater.inflate(R.layout.custom_toast_red,
+                    (ViewGroup) findViewById(R.id.layout_base));
+
+
+        }else if(estatus==5){
+            //Falla de red
+            layout = inflater.inflate(R.layout.custom_toast_sin_data,
+                    (ViewGroup) findViewById(R.id.layout_base));
+
+
+        }else{
+            //Informacion
+            layout = inflater.inflate(R.layout.custom_toast_info,
+                    (ViewGroup) findViewById(R.id.layout_base));
+        }
+
+        TextView textView = layout.findViewById(R.id.txtToast);
+        textView.setText(txt);
+        Toast toast = new Toast(this);
+        toast.setGravity(Gravity.CENTER_VERTICAL | Gravity.BOTTOM, 0, 50);
+        if(corto){
+            toast.setDuration(Toast.LENGTH_SHORT);}else{
+            toast.setDuration(Toast.LENGTH_LONG);}
+        toast.setView(layout);
+        toast.show();
     }
 }
