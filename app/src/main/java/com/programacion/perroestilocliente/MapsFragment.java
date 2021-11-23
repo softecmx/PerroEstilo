4f21c0baca93b02c7441380b349aa4af15ca1bbb
KevinSanchez-Cat <por_salon@hotmail.com>
2021-11-23 13:09:31 -0600

    Mapa google

--- app/src/main/java/com/programacion/perroestilocliente/MapsFragment.java
+++ app/src/main/java/com/programacion/perroestilocliente/MapsFragment.java
@@ -1,13 +1,10 @@
-package com.programacion.perroestilocliente.ui.cliente.direcciones.agregarDireccion;
-
-import androidx.lifecycle.ViewModelProvider;
-
-import android.os.Bundle;
+package com.programacion.perroestilocliente;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,38 +15,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.programacion.perroestilocliente.R;
 
-public class AgregarDireccionFragment extends Fragment {
+public class MapsFragment extends Fragment {
 
-    private AgregarDireccionViewModel mViewModel;
-
-    public static AgregarDireccionFragment newInstance() {
-        return new AgregarDireccionFragment();
-    }
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_agregar_direccion, container, false);
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(AgregarDireccionViewModel.class);
-        // TODO: Use the ViewModel
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        SupportMapFragment mapFragment =
-                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(callback);
-        }
-    }
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
         /**
@@ -65,9 +33,26 @@ public class AgregarDireccionFragment extends Fragment {
         public void onMapReady(GoogleMap googleMap) {
             LatLng sydney = new LatLng(-34, 151);
             googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            // googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+           // googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
             googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,13));
         }
     };
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_maps, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
+    }
 }
