afc902ae7b1b60fe6ba51ef606d886f8be772a8f
KevinSanchez-Cat <por_salon@hotmail.com>
2021-11-27 19:19:36 -0600

    Direccion y productos FIrebase

--- app/src/main/java/com/programacion/perroestilocliente/ui/cliente/direcciones/agregarDireccion/AgregarDireccionFragment.java
+++ app/src/main/java/com/programacion/perroestilocliente/ui/cliente/direcciones/agregarDireccion/AgregarDireccionFragment.java
@@ -1,20 +1,35 @@
 package com.programacion.perroestilocliente.ui.cliente.direcciones.agregarDireccion;
 
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Looper;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -23,6 +38,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.programacion.perroestilocliente.R;
 
 public class AgregarDireccionFragment extends Fragment implements OnMapReadyCallback {
@@ -32,6 +49,10 @@ public class AgregarDireccionFragment extends Fragment implements OnMapReadyCall
 
     MapView mapView;
     Marker marcador;
+    float txtLongitud;
+    float txtLatitud;
+    FusedLocationProviderClient client;
+
 
     /*
     Modificar la ruta
@@ -56,10 +77,30 @@ public class AgregarDireccionFragment extends Fragment implements OnMapReadyCall
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View root=inflater.inflate(R.layout.fragment_agregar_direccion, container, false);
+        View root = inflater.inflate(R.layout.fragment_agregar_direccion, container, false);
 
-        mapView = (MapView) root.findViewById(R.id.mapView);
+        mapView = (MapView) root.findViewById(R.id.mapViewAddDireccion);
+        int permissionCheck = ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            } else {
+                ActivityCompat.requestPermissions(null,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        1);
+            }
+        }
+
+
         initGoogleMap(savedInstanceState);
+        client = LocationServices.getFusedLocationProviderClient(getActivity());
+
+
+
+
+
+
 
         return root;
     }
@@ -71,6 +112,16 @@ public class AgregarDireccionFragment extends Fragment implements OnMapReadyCall
         // TODO: Use the ViewModel
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 100 && (grantResults.length > 0) &&
+                (grantResults[0] + grantResults[1] == PackageManager.PERMISSION_GRANTED)) {
+            getCurrentLocation();
+        } else {
+            Toast.makeText(getActivity(), "Permiso no definido", Toast.LENGTH_LONG).show();
+        }
+    }
 
     private void initGoogleMap(Bundle savedInstanceState) {
 
@@ -96,7 +147,6 @@ public class AgregarDireccionFragment extends Fragment implements OnMapReadyCall
 
         mapView.onSaveInstanceState(mapViewBundle);
     }
-
 
 
     @Override
@@ -152,4 +202,47 @@ public class AgregarDireccionFragment extends Fragment implements OnMapReadyCall
         }
         gMap.setMyLocationEnabled(true);
     }
+
+
+    @SuppressLint("MissingPermission")
+    private void getCurrentLocation() {
+
+        LocationManager locationManager = (LocationManager) getActivity()
+                .getSystemService(Context.LOCATION_SERVICE);
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(locationManager.NETWORK_PROVIDER)) {
+
+            client.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                    Location location = task.getResult();
+                    if (location != null) {
+                        txtLatitud = Float.parseFloat(String.valueOf(location.getLatitude()));
+                        txtLongitud = Float.parseFloat(String.valueOf(location.getLongitude()));
+                    } else {
+                        LocationRequest locationRequest = new LocationRequest()
+                                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                                .setInterval(1000)
+                                .setFastestInterval(1000)
+                                .setNumUpdates(1);
+                        LocationCallback locationCallback = new LocationCallback() {
+                            @Override
+                            public void onLocationResult(LocationResult locationResult) {
+                                Location location1 = locationResult.getLastLocation();
+                                txtLatitud = Float.parseFloat(String.valueOf(location.getLatitude()));
+                                txtLongitud = Float.parseFloat(String.valueOf(location.getLongitude()));
+                            }
+                        };
+
+                        client.requestLocationUpdates(locationRequest,
+                                locationCallback, Looper.myLooper());
+                    }
+                }
+            });
+        } else {
+            startActivity(new Intent(Settings.ACTION_LOCALE_SETTINGS)
+                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        }
+    }
+
 }
--- app/src/main/java/com/programacion/perroestilocliente/ui/cliente/productos/todosLosProductos/RViewAdapter.java
+++ app/src/main/java/com/programacion/perroestilocliente/ui/cliente/productos/todosLosProductos/RViewAdapter.java
@@ -2,12 +2,16 @@ package com.programacion.perroestilocliente.ui.cliente.productos.todosLosProduct
 
 import android.content.Context;
 import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentContainerView;
@@ -18,7 +22,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.programacion.perroestilocliente.R;
+import com.programacion.perroestilocliente.modelo.Productos;
 import com.programacion.perroestilocliente.ui.cliente.productos.verProductoTienda.VerProductoTiendaFragment;
 
 import java.util.ArrayList;
@@ -26,10 +35,11 @@ import java.util.ArrayList;
 public class RViewAdapter extends RecyclerView.Adapter<RViewAdapter.DataObjectHolder> {
 
     private Context context;
-    private ArrayList<Producto> listaProductos;
+    private ArrayList<Productos> listaProductos;
     FragmentManager fragmentManager;
     FragmentContainerView container;
-    public RViewAdapter(Context context, ArrayList<Producto> listaProductos, FragmentManager fragmentManager) {
+    private StorageReference storageReference = FirebaseStorage.getInstance().getReference("Productos");
+    public RViewAdapter(Context context, ArrayList<Productos> listaProductos, FragmentManager fragmentManager) {
         this.context = context;
         this.listaProductos = listaProductos;
         this.fragmentManager=fragmentManager;
@@ -46,12 +56,55 @@ public class RViewAdapter extends RecyclerView.Adapter<RViewAdapter.DataObjectHo
 
     @Override
     public void onBindViewHolder(@NonNull final DataObjectHolder holder, int position) {
+        holder.txtNombre.setText(listaProductos.get(position).getNombre());
 
-        holder.txtNombre.setText(listaProductos.get(position).getNombreProducto());
-        holder.txtDescuento.setPaintFlags(holder.txtDescuento.getPaintFlags()| Paint.STRIKE_THRU_TEXT_FLAG);
-        Glide.with(context).load(listaProductos.get(position).getImg()).into(holder.img);
+        String raiting = listaProductos.get(position).getRaiting();
+        float raitingStar = 0;
+        if (raiting.isEmpty()) {
+            raitingStar = 0;
+        } else {
+            raitingStar = Float.parseFloat(raiting);
+        }
+        holder.ratingBar.setRating(raitingStar);
 
-        holder.img.setOnClickListener(new View.OnClickListener() {
+        if (listaProductos.get(position).getDescuento().equals("") || listaProductos.get(position).getDescuento().equals("0")) {
+            holder.txtDescuento.setVisibility(View.GONE);
+            holder.txtOferta.setVisibility(View.GONE);
+            holder.txtPrecio.setText("$" + listaProductos.get(position).getPrecioVenta());
+        } else {
+            holder.txtOferta.setVisibility(View.VISIBLE);
+            holder.txtDescuento.setVisibility(View.VISIBLE);
+            float descuento=Float.parseFloat(listaProductos.get(position).getDescuento());
+
+            float precio=Float.parseFloat(listaProductos.get(position).getPrecioVenta());
+            float descuentoReal=(descuento*precio)/100;
+            float precioActual=precio-descuentoReal;
+            float redondeo=Math.round(precioActual);
+            holder.txtDescuento.setPaintFlags(holder.txtDescuento.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+            holder.txtDescuento.setText("$" + listaProductos.get(position).getPrecioVenta());
+            holder.txtPrecio.setText("$" + redondeo);
+        }
+
+        //  Glide.with(context).load(listaProductos.get(position).getImgFoto()).into(holder.img);
+        storageReference.child(listaProductos.get(position).getImgFoto()).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Glide.with(context).load(uri).into(holder.img);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(context, "Ha ocurrido un error al leer la imagen", Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.btnAgregarCarrito.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Position: " +
+                        holder.getAdapterPosition(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.btnVer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 VerProductoTiendaFragment newFragment1= new VerProductoTiendaFragment();
@@ -61,7 +114,6 @@ public class RViewAdapter extends RecyclerView.Adapter<RViewAdapter.DataObjectHo
                 fragmentTransaction.replace(R.id.container_cliente, newFragment1);
                 fragmentTransaction.addToBackStack(null);
                 fragmentTransaction.commit();
-
             }
         });
 
@@ -77,13 +129,22 @@ public class RViewAdapter extends RecyclerView.Adapter<RViewAdapter.DataObjectHo
         private ImageView img;
         private TextView txtNombre;
         private TextView txtDescuento;
-
+        private TextView txtOferta;
+        private TextView txtPrecio;
+        private RatingBar ratingBar;
+        private Button btnVer;
+        private Button btnAgregarCarrito;
 
         public DataObjectHolder(@NonNull View itemView) {
             super(itemView);
             this.img = itemView.findViewById(R.id.imgCardProductoTienda);
             this.txtNombre = itemView.findViewById(R.id.txtCardNombreProductoTienda);
-            this.txtDescuento=itemView.findViewById(R.id.txtDescuentoProdTienda);
+            this.txtDescuento = itemView.findViewById(R.id.txtDescuentoProdTienda);
+            this.txtOferta = itemView.findViewById(R.id.txtCardOferta);
+            this.txtPrecio = itemView.findViewById(R.id.txtCardProdPrecio);
+            this.ratingBar = itemView.findViewById(R.id.raitingBarProductoTienda);
+            this.btnVer = itemView.findViewById(R.id.btnCardVerProd);
+            this.btnAgregarCarrito = itemView.findViewById(R.id.btnCardAgregarProd);
         }
     }
 
