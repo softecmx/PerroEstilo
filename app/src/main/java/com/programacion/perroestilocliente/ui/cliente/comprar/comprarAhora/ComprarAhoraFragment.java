ee8b49a43b158a824aa58164698b15d9798dc52f
KevinSanchez-Cat <por_salon@hotmail.com>
2021-11-30 08:47:50 -0600

    confirmar compra y procesar pedido

--- app/src/main/java/com/programacion/perroestilocliente/ui/cliente/comprar/comprarAhora/ComprarAhoraFragment.java
+++ app/src/main/java/com/programacion/perroestilocliente/ui/cliente/comprar/comprarAhora/ComprarAhoraFragment.java
@@ -4,6 +4,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,27 +14,41 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.programacion.perroestilocliente.R;
 import com.programacion.perroestilocliente.bd.Item;
+import com.programacion.perroestilocliente.modelo.Clientes;
+import com.programacion.perroestilocliente.modelo.DetOrdenProductos;
+import com.programacion.perroestilocliente.modelo.Direcciones;
+import com.programacion.perroestilocliente.modelo.OrdenesCliente;
+import com.programacion.perroestilocliente.ui.cliente.comprar.ElementListView;
+import com.programacion.perroestilocliente.ui.cliente.comprar.ListAdapterSimple;
 import com.programacion.perroestilocliente.ui.cliente.comprar.confirmarCompra.ConfirmarCompraFragment;
 import com.programacion.perroestilocliente.ui.cliente.mainCliente.ListAdapterCarrito;
 
-import org.w3c.dom.Text;
-
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+import java.util.UUID;
 
 public class ComprarAhoraFragment extends Fragment {
     float total;
@@ -49,7 +64,15 @@ public class ComprarAhoraFragment extends Fragment {
     TextView txtCalleExterior;
     TextView txtCalleInterior;
     TextView txtReferencias;
-
+    ArrayList<Item> arrayListItems;
+    AutoCompleteTextView spinComAhEscogerDirecc;
+    private ListAdapterSimple customAdapterDric;
+    ArrayList<ElementListView> arrayList;
+    String dir = "";
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private StorageReference storageReference;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     private ComprarAhoraViewModel mViewModel;
     View root;
@@ -62,9 +85,19 @@ public class ComprarAhoraFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.fragment_comprar_ahora, container, false);
-
+        spinComAhEscogerDirecc=root.findViewById(R.id.spinComAhEscogerDirecc);
         btnComprarAhoraCancelar = root.findViewById(R.id.btnComprarAhoraCancelar);
         btnComprarAhoraConfirmar = root.findViewById(R.id.btnComprarAhoraConfirmar);
+        txtNombre=root.findViewById(R.id.txtCompAhoNomCont);
+        txtApellidos=root.findViewById(R.id.txtCompAhoApellidoPCont);
+        txtTelefono=root.findViewById(R.id.txtCompAhoTelefonoCont);
+        txtEstado=root.findViewById(R.id.txtCompAhEstado);
+        txtMunicipio=root.findViewById(R.id.txtCompAhMunicipio);
+        txtLocalidad=root.findViewById(R.id.txtCompAhLocalidad);
+        txtCodigoPostal=root.findViewById(R.id.txtCompAhCodPostal);
+        txtCalleExterior=root.findViewById(R.id.txtCompAhCalleExt);
+        txtCalleInterior=root.findViewById(R.id.txtCompAhCalleInt);
+        txtReferencias=root.findViewById(R.id.txtCompAhReferencias);
 
         FirebaseDatabase firebaseDatabase;
         DatabaseReference databaseReference;
@@ -74,12 +107,66 @@ public class ComprarAhoraFragment extends Fragment {
         storageReference = FirebaseStorage.getInstance().getReference("Productos");
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
+        Query queryCliente = databaseReference.child("Usuarios/Clientes").orderByChild("email").equalTo(user.getEmail());
+        queryCliente.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                if (snapshot.exists()) {
+                                    for (DataSnapshot objSnapshot : snapshot.getChildren()) {
+                                        Clientes usuario = objSnapshot.getValue(Clientes.class);
+
+                                        txtNombre.setText(usuario.getNombreCliente());
+                                        txtApellidos.setText(usuario.getApellidoPaterno());
+                                        txtTelefono.setText(usuario.getTelefono());
+                                    }
+                                }
+                            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        databaseReference.child("Direcciones").orderByChild("idUser").equalTo(user.getUid()).
+                addListenerForSingleValueEvent(new ValueEventListener(){
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot){
+                arrayList = new ArrayList<>();
+                spinComAhEscogerDirecc.setAdapter(null);
+                for (DataSnapshot objSnapshot: snapshot.getChildren()){
+                    Direcciones p = objSnapshot.getValue(Direcciones.class);
+                    arrayList.add(new ElementListView(p.getIdDireccion(),p.getEntidadFederativa(),
+                            p.getMunicipio(),p.getLocalidad(),p.getCalleYNumeroExterno(),p.getCalleYNumeroInterno(),p.getReferencia(),
+                            p.getCodigoPostal()));
+                    customAdapterDric = new ListAdapterSimple(getActivity(), arrayList);
+                    spinComAhEscogerDirecc.setAdapter(customAdapterDric);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error){
+
+            }
+        });
+
+        spinComAhEscogerDirecc.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //dir=customAdapterDric.getItem(position).getIdDireccion();
+                txtEstado.setText(customAdapterDric.getItem(position).getEntidadFederativa());
+                txtMunicipio.setText(customAdapterDric.getItem(position).getMunicipio());
+                txtLocalidad.setText(customAdapterDric.getItem(position).getLocalidad());
+                txtCodigoPostal.setText(customAdapterDric.getItem(position).getCodigoPostal());
+                txtCalleExterior.setText(customAdapterDric.getItem(position).getCalleYNumeroExterno());
+                txtCalleInterior.setText(customAdapterDric.getItem(position).getCalleYNumeroInterno());
+                txtReferencias.setText(customAdapterDric.getItem(position).getReferencia());
+            }
+        });
         // btnPopCerrar = (Button) aboutPop.findViewById(R.id.btnCerrarDialog);
         TextView txtTotal = root.findViewById(R.id.txtComprarAhoraTotalPagar);
         ListView reciclerViewMiCarritoProductos = root.findViewById(R.id.lstViewComprarAhoraCarrito);
-        ArrayList<Item> arrayListItems = new ArrayList<>();
+         arrayListItems = new ArrayList<>();
         total = 0;
-        databaseReference.child("Carrito/" + user.getUid() + "/Items").addValueEventListener(new ValueEventListener() {
+        databaseReference.child("Carrito/" + user.getUid() + "/Items").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 arrayListItems.clear();
@@ -111,19 +198,13 @@ public class ComprarAhoraFragment extends Fragment {
         btnComprarAhoraConfirmar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ConfirmarCompraFragment newFragment1 = new ConfirmarCompraFragment();
-                Bundle args = new Bundle();
-                newFragment1.setArguments(args);
-                FragmentManager fragmentManager = getFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.replace(R.id.container_cliente, newFragment1);
-                fragmentTransaction.addToBackStack(null);
-                fragmentTransaction.commit();
+                realizarPedido();
             }
         });
         btnComprarAhoraCancelar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                databaseReference.child("Carrito").child(user.getUid()).removeValue();
                 FragmentManager fragmentManager = getFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 fragmentTransaction.remove(ComprarAhoraFragment.this);
@@ -142,4 +223,76 @@ public class ComprarAhoraFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
+    public void realizarPedido(){
+
+        String fechaPedido=new SimpleDateFormat("dd/MM/yyyy").format(new Date());
+        String idCliente=user.getUid();
+        String idOrden= (UUID.randomUUID().toString()).subSequence(0,10).toString();
+        String estatus="Pago pendiente";
+        String noSerie1=UUID.randomUUID().toString();
+        String noSerie2="";
+        char[] toChar=noSerie1.toCharArray();
+        for (int i=0; i<toChar.length;i++){
+            if(toChar[i]=='-'){
+
+            }else{
+                noSerie2+=toChar[i];
+            }
+        }
+        noSerie1=noSerie2.substring(0,12).toUpperCase();
+        String noConfirm="0";
+        OrdenesCliente ordenCliente=new OrdenesCliente();
+        ordenCliente.setInOrden(idOrden);
+        ordenCliente.setIdCliente(idCliente);
+        ordenCliente.setEstatusOrden(estatus);
+        ordenCliente.setNoSerie(noSerie1);
+        ordenCliente.setFechaOrden(fechaPedido);
+        ordenCliente.setNombreContacto(txtNombre.getText().toString());
+        ordenCliente.setApPContacto(txtApellidos.getText().toString());
+        ordenCliente.setApMContacto("");
+        ordenCliente.setTelefonoContacto(txtTelefono.getText().toString());
+        ordenCliente.setFechaEntrega("");
+        Direcciones direccionEnvio=new Direcciones();
+        direccionEnvio.setEntidadFederativa(txtEstado.getText().toString());
+        direccionEnvio.setMunicipio(txtMunicipio.getText().toString());
+        direccionEnvio.setLocalidad(txtLocalidad.getText().toString());
+        direccionEnvio.setCodigoPostal(txtCodigoPostal.getText().toString());
+        direccionEnvio.setCalleYNumeroExterno(txtCalleExterior.getText().toString());
+        direccionEnvio.setCalleYNumeroInterno(txtCalleInterior.getText().toString());
+        direccionEnvio.setReferencia(txtReferencias.getText().toString());
+        ordenCliente.setDireccionEnvio(direccionEnvio);
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference();
+
+        ref.child("OrdenesCliente/"+user.getUid()).child(ordenCliente.getInOrden()).setValue(ordenCliente);
+
+        for (int i=0; i<arrayListItems.size();i++){
+            DetOrdenProductos detOrdenProd=new DetOrdenProductos();
+            detOrdenProd.setIdDetOrdenProducto(UUID.randomUUID().toString());
+            detOrdenProd.setIdOrdenCliente(ordenCliente.getInOrden());
+            detOrdenProd.setIdProducto(arrayListItems.get(i).getProducto());
+            detOrdenProd.setImgFoto(arrayListItems.get(i).getImg());
+            detOrdenProd.setCantidad(arrayListItems.get(i).getCantidad()+"");
+            detOrdenProd.setSubtotal((arrayListItems.get(i).getCantidad()*arrayListItems.get(i).getPrecio())+"");
+            detOrdenProd.setMedidas(arrayListItems.get(i).getTalla());
+            detOrdenProd.setCalificacion("0");
+            ref.child("DetalleOrdenesCliente/"+ordenCliente.getInOrden()).child(detOrdenProd.getIdDetOrdenProducto()).setValue(detOrdenProd);
+        }
+        float total=0;
+        for (int i=0;i<arrayListItems.size();i++){
+            total=total+(arrayListItems.get(i).getCantidad()*arrayListItems.get(i).getPrecio());
+        }
+        ref.child("Carrito").child(user.getUid()).removeValue();
+        ConfirmarCompraFragment newFragment1 = new ConfirmarCompraFragment();
+        Bundle args = new Bundle();
+        args.putString("noOrden",idOrden);
+        args.putString("fechaOrden",fechaPedido);
+        args.putFloat("total",total);
+        newFragment1.setArguments(args);
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container_cliente, newFragment1);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
 }
--- app/src/main/java/com/programacion/perroestilocliente/ui/cliente/comprar/confirmarCompra/ConfirmarCompraFragment.java
+++ app/src/main/java/com/programacion/perroestilocliente/ui/cliente/comprar/confirmarCompra/ConfirmarCompraFragment.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.programacion.perroestilocliente.R;
 import com.programacion.perroestilocliente.ui.cliente.pedidos.misPedidos.MisPedidosFragment;
@@ -25,6 +26,15 @@ public class ConfirmarCompraFragment extends Fragment {
     Button btnVerTicket;
     Button btnVerMisPedidos;
     View root;
+    String fechaPedido;
+    String idOrden;
+    float totalApagar;
+
+    TextView fechaEmision;
+    TextView fechaLimite;
+    TextView total;
+    TextView concepto;
+
     public static ConfirmarCompraFragment newInstance() {
         return new ConfirmarCompraFragment();
     }
@@ -32,15 +42,42 @@ public class ConfirmarCompraFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        root=inflater.inflate(R.layout.fragment_confirmar_compra, container, false);
-        btnVerTicket=root.findViewById(R.id.btnVerTicketCompra);
-        btnVerMisPedidos=root.findViewById(R.id.btnVerMisPedidosCompra);
+        root = inflater.inflate(R.layout.fragment_confirmar_compra, container, false);
+
+        Bundle arg = getArguments();
+        idOrden = arg.getString("noOrden");
+        fechaPedido = arg.getString("fechaOrden");
+        totalApagar = arg.getFloat("total");
+
+        fechaEmision = root.findViewById(R.id.txtConfCompraFechaEmisionCompra);
+        fechaLimite = root.findViewById(R.id.txtConfirmarCompraFechaLimiteCompra);
+        total = root.findViewById(R.id.txtConfirmarCompraTotalPagarCompra);
+        concepto = root.findViewById(R.id.txtConceptoTransCompra);
+
+        fechaEmision.setText(fechaPedido);
+        String []fecha=fechaPedido.split("/");
+        String anio=fecha[2];
+        String mes=fecha[1];
+        String dia=fecha[0];
+
+        int anioF=Integer.parseInt(anio);
+        int mesF=Integer.parseInt(mes);
+        int diaF=Integer.parseInt(dia);
+
+        String fechaLimit=(dia+3)+"/"+mes+"/"+anio;
+        fechaLimite.setText(fechaLimit);
+        total.setText("$"+totalApagar+" MX");
+        concepto.setText("Perro estilo. No. Pedido: "+idOrden);
+
+        btnVerTicket = root.findViewById(R.id.btnVerTicketCompra);
+        btnVerMisPedidos = root.findViewById(R.id.btnVerMisPedidosCompra);
 
         btnVerTicket.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 TicketFragment newFragment1 = new TicketFragment();
                 Bundle args = new Bundle();
+                args.putString("noOrden",idOrden);
                 newFragment1.setArguments(args);
                 FragmentManager fragmentManager = getFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
